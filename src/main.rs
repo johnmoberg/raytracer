2d7fc2ea6b4dc177f38c4f1d78fc18afc34d5960
John Moberg <john@depict.ai>
2022-07-26 22:59:05 +0200

    PPM writer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fmt::Write;
+
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    let mut content = format!("P3\n {} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f32) / (IMAGE_WIDTH - 1) as f32;
+            let g = (j as f32) / (IMAGE_HEIGHT - 1) as f32;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            writeln!(content, "{} {} {}", ir, ig, ib).unwrap();
+        }
+    }
+
+    print!("{}", content);
 }
