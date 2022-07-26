49d5e5f6655e68559194f420f7745faa297b7625
John Moberg <john@depict.ai>
2022-07-26 23:43:03 +0200

    Blue-to-white gradient

--- src/main.rs
+++ src/main.rs
@@ -24,22 +24,45 @@ impl Ray {
     fn at(&self, t: f32) -> Vector3<f32> {
         self.origin + t * self.direction
     }
+
+    fn color(&self) -> impl Color {
+        let normalized_direction = self.direction.normalize();
+        let t = 0.5 * (normalized_direction.y + 1.0);
+        (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0)
+    }
 }
 
 fn main() {
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    // Image
+    const ASPECT_RATIO: f32 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 400;
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as i32;
 
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vector3::new(0.0, 0.0, 0.0);
+    let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vector3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vector3::new(0.0, 0.0, focal_length);
+
+    // Render
     let mut content = format!("P3\n {} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scanlines remaining: {}", j);
 
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f32) / (IMAGE_WIDTH - 1) as f32;
-            let g = (j as f32) / (IMAGE_HEIGHT - 1) as f32;
-            let b = 0.25;
-            let color = Vector3::new(r, g, b);
+            let u = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let v = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let ray = Ray {
+                origin,
+                direction: lower_left_corner + u * horizontal + v * vertical - origin,
+            };
+            let color = ray.color();
 
             content.push_str(&Color::to_string(&color));
         }
