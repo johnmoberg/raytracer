39e25a0e5aa7b01a1bfb855327354d2e437226c5
John Moberg <john@depict.ai>
2022-07-26 23:20:30 +0200

    Use Vector from nalgebra for colors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-use std::fmt::Write;
+extern crate nalgebra as na;
+use na::Vector3;
+
+trait Color {
+    fn to_string(&self) -> String;
+}
+
+impl Color for Vector3<f32> {
+    fn to_string(&self) -> String {
+        let ir = (255.999 * self.x) as i32;
+        let ig = (255.999 * self.y) as i32;
+        let ib = (255.999 * self.z) as i32;
+
+        format!("{} {} {}\n", ir, ig, ib)
+    }
+}
 
 fn main() {
     const IMAGE_WIDTH: i32 = 256;
@@ -13,12 +28,9 @@ fn main() {
             let r = (i as f32) / (IMAGE_WIDTH - 1) as f32;
             let g = (j as f32) / (IMAGE_HEIGHT - 1) as f32;
             let b = 0.25;
+            let color = Vector3::new(r, g, b);
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            writeln!(content, "{} {} {}", ir, ig, ib).unwrap();
+            content.push_str(&Color::to_string(&color));
         }
     }
 
