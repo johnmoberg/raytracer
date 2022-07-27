e2e60d240bea3e473a3dde15f33d0930ea5eb0e4
John Moberg <john@depict.ai>
2022-07-27 20:08:04 +0200

    Color depending on surface normal

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,10 @@ impl Ray {
     }
 
     fn color(&self) -> impl Color {
-        if hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, &self) {
-            return Vector3::new(1.0, 0.0, 0.0);
+        let t = hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, self);
+        if t > 0.0 {
+            let n = self.at(t) - Vector3::new(0.0, 0.0, -1.0);
+            return 0.5 * Vector3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
         }
 
         let normalized_direction = self.direction.normalize();
@@ -36,14 +38,18 @@ impl Ray {
     }
 }
 
-fn hit_sphere(center: Vector3<f32>, radius: f32, ray: &Ray) -> bool {
+fn hit_sphere(center: Vector3<f32>, radius: f32, ray: &Ray) -> f32 {
     let oc = ray.origin - center;
     let a = ray.direction.dot(&ray.direction);
     let b = 2.0 * oc.dot(&ray.direction);
     let c = oc.dot(&oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn main() {
